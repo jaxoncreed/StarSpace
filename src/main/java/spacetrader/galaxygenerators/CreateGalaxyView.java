9e0c383fda9a9bd1682417d0611888b7eae1a1f5
Francisco <fmsanchez001@gmail.com>
2014-10-08 00:03:02 -0400

    Galaxy view load screen.

--- src/main/java/spacetrader/galaxygenerators/CreateGalaxyView.java
+++ src/main/java/spacetrader/galaxygenerators/CreateGalaxyView.java
@@ -6,6 +6,18 @@ package spacetrader.galaxygenerators;
  * and open the template in the editor.
  */
 import spacetrader.Window;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import spacetrader.AbstractView;
 
 /**
  *
@@ -14,9 +26,23 @@ import spacetrader.Window;
 public class CreateGalaxyView {
     Window window;
     GalaxyGeneratorCtrl createGalaxyCtrl;
+    Pane pane;
     
     public CreateGalaxyView(Window window, GalaxyGeneratorCtrl ccc) {
         this.window = window;
         createGalaxyCtrl = ccc;
+        pane = new Pane();
+    }
+    
+    public void renderGalaxyCreator() {
+        try {
+            window.loadFXML(pane);
+        } catch (IOException ex) {
+            Logger.getLogger(CreateGalaxyView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void removeGalaxyCreator() {
+        window.clearFXML(pane);
     }
 }

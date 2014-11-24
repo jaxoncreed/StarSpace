1a108a002a1c2226e99092822a725c592fa4843a
jaxoncreed <jaxoncreed@gmail.com>
2014-11-24 05:14:17 -0500

    added some temporation items to populate the marketplace

--- src/main/java/spacetrader/galaxygenerators/SimplePlanetGenerator.java
+++ src/main/java/spacetrader/galaxygenerators/SimplePlanetGenerator.java
@@ -1,8 +1,7 @@
 package spacetrader.galaxygenerators;
 
+import spacetrader.game_model.gameLogic.Item;
 import spacetrader.game_model.system.Planet;
-import spacetrader.game_model.gameLogic.Position;
-import spacetrader.game_model.system.StarSystem;
 //import spacetrader.Util;
 
 /**
@@ -11,6 +10,25 @@ import spacetrader.game_model.system.StarSystem;
 public class SimplePlanetGenerator extends PlanetGenerator {
     
 	public Planet generate() {
-		return new Planet(name, pos);
+                //TODO: actual item generation
+                Planet p = new Planet(name, pos);
+                p.getMarket().getCargo().setMaxSize(100);
+                p.getMarket().addItem(new Item("Kool-Aid", 22.23));
+                p.getMarket().addItem(new Item("Kool-Aid", 22.23));
+                p.getMarket().addItem(new Item("Kool-Aid", 22.23));
+                p.getMarket().addItem(new Item("Kool-Aid", 22.23));
+                p.getMarket().addItem(new Item("Nissan Leaf", 21510.00));
+                p.getMarket().addItem(new Item("Nissan Leaf", 21510.00));
+                p.getMarket().addItem(new Item("Domain Name", 56.99));
+                p.getMarket().addItem(new Item("Christopher Nolan's Interstellar", -5.00));
+                p.getMarket().addItem(new Item("Christopher Nolan's Interstellar", -5.00));
+                p.getMarket().addItem(new Item("Christopher Nolan's Interstellar", -5.00));
+                p.getMarket().addItem(new Item("Stanley Kubrick's 2001 A Space Odyssey", 496218.99));
+                p.getMarket().addItem(new Item("Day with Kim Jong Un!", 98.00));
+                p.getMarket().addItem(new Item("Some Sleep", 36.42));
+                p.getMarket().addItem(new Item("Some more Sleep", 45.96));
+                p.getMarket().addItem(new Item("A Hug", 2.00));
+                
+		return p;
 	}
 }
